828ebd84714d2e7c5dbec6a70953df1950abfe5f
rezachris <rezachrismardianto@ymail.com>
2019-03-21 17:09:57 +0700

    initial commit

---
+++ larastart.sql
@@ -0,0 +1,129 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Mar 19, 2019 at 03:53 AM
+-- Server version: 10.1.33-MariaDB
+-- PHP Version: 7.2.6
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `larastart`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `migrations`
+--
+
+CREATE TABLE `migrations` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `migration` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `batch` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `migrations`
+--
+
+INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
+(1, '2014_10_12_000000_create_users_table', 1),
+(2, '2014_10_12_100000_create_password_resets_table', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `password_resets`
+--
+
+CREATE TABLE `password_resets` (
+  `email` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `token` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `name` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email_verified_at` timestamp NULL DEFAULT NULL,
+  `password` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `type` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT 'user',
+  `bio` mediumtext COLLATE utf8mb4_unicode_ci,
+  `photo` varchar(191) COLLATE utf8mb4_unicode_ci DEFAULT 'profile.png',
+  `remember_token` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `name`, `email`, `email_verified_at`, `password`, `type`, `bio`, `photo`, `remember_token`, `created_at`, `updated_at`) VALUES
+(1, 'Reza Chrismardianto', 'rezachrismardianto@ymail.com', NULL, '$2y$10$74SP82BwJtwS95KEwEinSeWNLJhlmu/3BkMhhoOTMagsyKZjNTcLK', 'user', NULL, 'profile.png', NULL, '2019-03-18 17:08:16', '2019-03-18 17:08:16'),
+(4, 'Reza Chrismardianto', 'paramithadewi75@yahoo.com', NULL, '$2y$10$vdgF7663WcutjKvLKGCdXusp0nYBHVMYOQzHs7zG21ERhZTkCc3q.', 'admin', NULL, NULL, NULL, '2019-03-18 17:19:16', '2019-03-18 17:19:16'),
+(5, 'Reza Chrismardianto', 'elenor.ullrich@example.net', NULL, '$2y$10$A1RNkttg0tcHiJN4vwwEHOF/6HYcwX33812mIush4XfY.r0oQTllO', 'user', NULL, NULL, NULL, '2019-03-18 17:19:50', '2019-03-18 17:19:50'),
+(6, 'sss', 'aaa@ymail.com', NULL, '$2y$10$Gc3.EplalmBmo0C6MrMS8e86bCR9lZYGQiPhuSL0dIvtHGJTLl9g6', 'user', 'sss', NULL, NULL, '2019-03-18 19:51:34', '2019-03-18 19:51:34');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `migrations`
+--
+ALTER TABLE `migrations`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `password_resets`
+--
+ALTER TABLE `password_resets`
+  ADD KEY `password_resets_email_index` (`email`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `users_email_unique` (`email`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `migrations`
+--
+ALTER TABLE `migrations`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
---
+++ resources/js/components/Users.vue
@@ -0,0 +1,212 @@
+<template>
+    <div class="container">
+       <div class="row mt-5">
+          <div class="col-md-12">
+            <div class="card">
+              <div class="card-header">
+                <h3 class="card-title">Users Table</h3>
+
+                <div class="card-tools">
+                  <button class="btn btn-success" @click="newModal">Add New <i class="fas fa-user-plus fa-fwn "></i></button>
+                </div>
+
+              </div>
+              <!-- /.card-header -->
+              <div class="card-body table-responsive p-0">
+                <table class="table table-hover">
+                  <tbody><tr>
+                    <th>ID</th>
+                    <th>Name</th>
+                    <th>Email</th>
+                    <th>Type</th>
+                    <th>Registered At</th>
+                    <th>Action</th>
+                  </tr>
+                  <tr v-for="user in users" :key="user.id">
+                    <td>{{ user.id }}</td>
+                    <td>{{ user.name }}</td>
+                    <td>{{ user.email }}</td>
+                    <td>{{ user.type | upText }}</td>
+                    <td>{{ user.created_at | myDate }}</td>
+                    <td>
+                        <a href="#" @click="editModal(user)">
+                            <i class="fa fa-edit blue"></i>
+                        </a>
+                        /
+                        <a href="#" @click="deleteUser(user.id)">
+                            <i class="fa fa-trash red"></i>
+                        </a>
+                    </td>
+                  </tr>
+                </tbody></table>
+              </div>
+              <!-- /.card-body -->
+            </div>
+            <!-- /.card -->
+          </div>
+        </div>
+        <!-- Modal -->
+        <div class="modal fade" id="addNew" tabindex="-1" role="dialog" aria-labelledby="addNewLabel" aria-hidden="true">
+          <div class="modal-dialog modal-dialog-centered" role="document">
+            <div class="modal-content">
+              <div class="modal-header">
+                <h5 class="modal-title" id="addNewLabel" v-show="!editmode">Add New</h5>
+                <h5 class="modal-title" id="addNewLabel" v-show="editmode">Update User</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                  <span aria-hidden="true">&times;</span>
+                </button>
+              </div>
+              <form @submit.prevent="editmode ? updateUser() : createUser()">
+              <div class="modal-body">
+                 <div class="form-group">
+                     <input v-model="form.name" type="text" name="name" class="form-control" :class="{ 'is-invalid':form.errors.has('name') }" placeholder="Name">
+                     <has-error :form="form" field="name"></has-error>
+                 </div>
+                 <div class="form-group">
+                     <input v-model="form.email" type="email" name="email" class="form-control" :class="{ 'is-invalid':form.errors.has('email') }" placeholder="Email Address">
+                     <has-error :form="form" field="email"></has-error>
+                 </div>
+                 <div class="form-group">
+                     <textarea v-model="form.bio" name="bio" id="bio" class="form-control" :class="{ 'is-invalid':form.errors.has('bio') }" placeholder="Short bio for user (Optional)"></textarea>
+                     <has-error :form="form" field="bio"></has-error>
+                 </div>
+                 <div class="form-group">
+                     <select v-model="form.type" name="type" id="type" class="form-control" :class="{ 'is-invalid':form.errors.has('type') }">
+                        <option value="">Select User Role</option>
+                        <option value="admin">Admin</option>
+                        <option value="user">Standar User</option>
+                        <option value="author">Author</option>
+                     </select>
+                     <has-error :form="form" field="type"></has-error>
+                 </div>
+                 <div class="form-group">
+                     <input v-model="form.password" type="password" name="password" id="password" class="form-control" :class="{ 'is-invalid':form.errors.has('password') }">
+                     <has-error :form="form" field="password"></has-error>
+                 </div>
+              </div>
+              <div class="modal-footer">
+                <button type="button" class="btn btn-danger" data-dismiss="modal">Close</button>
+                <button v-show="editmode" type="submit" class="btn btn-success">Update</button>
+                <button v-show="!editmode" type="submit" class="btn btn-primary">Create</button>
+              </div>
+              </form>
+            </div>
+          </div>
+        </div>
+    </div>
+
+
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                editmode : false,
+                users: {},
+                form: new Form({
+                  id:'',
+                  name : '',
+                  email: '',
+                  password: '',
+                  type: '',
+                  bio: '',
+                  photo: ''
+                })
+            }
+        },
+        methods: {
+          updateUser(){
+            this.$Progress.start();
+
+            this.form.put('api/user/'+this.form.id)
+            .then(() => {
+                // success
+                $('#addNew').modal('hide');
+                Swal.fire(
+                    'Update!',
+                    'Your file has been uppdate.',
+                    'success'
+                )
+                this.$Progress.finish();
+                Fire.$emit('AfterCreate');
+            })
+            .catch(() => {
+                this.$Progress.fail();
+            })
+          },
+          editModal(user){
+            this.editmode = true;
+            this.form.reset();
+            $('#addNew').modal('show');
+            this.form.fill(user);
+          },
+          newModal(){
+            this.editmode = false;
+            this.form.reset();
+            $('#addNew').modal('show');
+          },
+          deleteUser(id){
+              Swal.fire({
+                  title: 'Are you sure?',
+                  text: "You won't be able to revert this!",
+                  type: 'warning',
+                  showCancelButton: true,
+                  confirmButtonColor: '#3085d6',
+                  cancelButtonColor: '#d33',
+                  confirmButtonText: 'Yes, delete it!'
+                  }).then((result) => {
+
+                      // Send request to the serve
+                      if (result.value) {
+                          this.form.delete('api/user/'+id).then(()=>{
+
+                              Swal.fire(
+                                'Deleted!',
+                                'Your file has been deleted.',
+                                'success'
+                              )
+                              Fire.$emit('AfterCreate');
+                          }).catch(()=>{
+                              Swal.fire(
+                                  'Failed!',
+                                  'There was something wrong',
+                                  'warning'
+                              );
+                          });
+                      }
+              })
+          },
+          loadUsers(){
+              axios.get('api/user').then(({data})=>(this.users = data.data));
+          },
+
+          createUser(){
+              this.$Progress.start();
+
+              this.form.post('api/user')
+              .then(()=>{
+                Fire.$emit('AfterCreate');
+                $('#addNew').modal('hide')
+
+                Toast.fire({
+                    type: 'success',
+                    title: 'User create in successfully'
+                })
+
+                this.$Progress.finish();
+              })
+              .catch(()=>{
+
+              })
+          }
+      },
+      created() {
+          this.loadUsers();
+          Fire.$on('AfterCreate',() => {
+              this.loadUsers()
+          });
+          // setInterval(() => this.loadUsers(), 3000);
+      }
+    };
+</script>
